006af727240e2f22045596e5eebe7e0acc47a86a
jbth23 <jbth@tutamail.com>
2025-01-27 13:48:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
